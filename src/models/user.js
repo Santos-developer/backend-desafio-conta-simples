dc895cd708cb7c8cce2aab1ae8dc84eb12e4062a
Santos-developer <santos.developer21@gmail.com>
2019-12-04 19:50:19 -0300

    (add) added user model

---
+++ src/models/user.js
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: { type: Schema.Types.String, required: true },
+    email: { type: Schema.Types.String, required: true, unique: true },
+    password: { type: Schema.Types.String, required: true },
+    picture: { type: Schema.Types.String },
+    digit: { type: Schema.Types.Number, required: true },
+    agency: { type: Schema.Types.Number, required: true },
+    total: { type: Schema.Types.Number, required: true, default: 0 },
+    rentability: { type: Schema.Types.Number, required: true, default: 0 },
+    entries: { type: Schema.Types.Number, required: true, default: 0 },
+    transactions: { type: Schema.Types.Array, required: true }
+  },
+  { timestamps: true }
+);
+
+module.exports = model("user", userSchema);
