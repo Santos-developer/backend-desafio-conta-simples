2e09bb8f1c1fef7f44daa57081f549e559446b99
Santos-developer <santos.developer21@gmail.com>
2019-12-04 19:55:59 -0300

    (add) added user controller

---
+++ src/controllers/user.js
@@ -0,0 +1,49 @@
+const userModel = require("../models/user");
+
+const create = async userData => {
+  try {
+    const newUser = new userModel(userData);
+
+    await newUser.validate();
+    await newUser.save();
+  } catch (error) {
+    return Promise.reject(error.message);
+  }
+
+  return 200;
+};
+
+const read = async userID => {
+  try {
+    return await userModel.findById(userID);
+  } catch (error) {
+    return Promise.reject(error.message);
+  }
+};
+
+const update = async (userID, userData) => {
+  try {
+    await userModel.findByIdAndUpdate(userID, userData);
+  } catch (error) {
+    return Promise.reject(error.message);
+  }
+
+  return 200;
+};
+
+const remove = async userID => {
+  try {
+    await userModel.findByIdAndDelete(userID);
+  } catch (error) {
+    return Promise.reject(error.message);
+  }
+
+  return 200;
+};
+
+module.exports = {
+  create,
+  read,
+  update,
+  remove
+};
