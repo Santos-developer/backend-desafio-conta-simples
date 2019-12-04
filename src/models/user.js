46098bc3dbd972a490f01093bef446451ad1bf48
Santos-developer <santos.developer21@gmail.com>
2019-12-04 20:10:36 -0300

    (add) added new property to user model

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
   {
+    admin: { type: Schema.Types.Boolean, default: false },
     name: { type: Schema.Types.String, required: true },
     email: { type: Schema.Types.String, required: true, unique: true },
     password: { type: Schema.Types.String, required: true },
